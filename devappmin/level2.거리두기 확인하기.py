3a587d596c31717cf03874bf483aadc51754f131
Kim Seung Hwan <dev.appmin@gmail.com>
2022-05-15 20:46:47 +0900

    refactor: 거리두기 확인하기

--- devappmin/level2.거리두기 확인하기.py
+++ devappmin/level2.거리두기 확인하기.py
@@ -5,19 +5,16 @@ dy, dx = (1, -1, 0, 0), (0, 0, 1, -1)
 def sub_solution(place):
     q = deque()
 
-    visited = [[0] * 5 for _ in range(5)]
-
-    count = 1
+    visited = [[False] * 5 for _ in range(5)]
     
     for y_idx in range(5):
         for x_idx in range(5):
             if place[y_idx][x_idx] == 'P':
-                visited[y_idx][x_idx] = count
-                q.append((count, y_idx, x_idx))
-                count += 1
+                visited[y_idx][x_idx] = True 
+                q.append((y_idx, x_idx))
     
     while q:
-        person, y, x = q.popleft()
+        y, x = q.popleft()
 
         for idx in range(4):
             ny, nx = y + dy[idx], x + dx[idx]
@@ -28,10 +25,10 @@ def sub_solution(place):
             if place[ny][nx] == 'X':
                 continue
             
-            if visited[ny][nx] != 0 and visited[ny][nx] != person:
+            if visited[ny][nx]:
                 return 0
             
-            visited[ny][nx] = person
+            visited[ny][nx] = True
 
     return 1
 
