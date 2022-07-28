0665a69d12713bd700b9dc7fcf050f36e7f2d6e7
이정훈 <dev.rookie37@gmail.com>
2022-07-28 16:49:01 +0900

    refactor: hack-rookie37/타겟 넘버

--- hack-rookie37/level2.타겟 넘버.py
+++ hack-rookie37/level2.타겟 넘버.py
@@ -6,14 +6,15 @@ d = [1, -1]
 def solution(numbers, target):
     answer = 0
     q = deque([(0, 0)])
+    L = len(numbers)
 
     while q:
         x, n = q.popleft()
 
-        if n == len(numbers) and x == target:
+        if n == L and x == target:
             answer += 1
 
-        if n < len(numbers):
+        if n < L:
             for k in range(2):
                 nx = x + numbers[n] * d[k]
                 q.append((nx, n + 1))
