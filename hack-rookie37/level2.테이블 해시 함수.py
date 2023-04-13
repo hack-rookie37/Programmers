4ef96efb3d3dffea03c637af73804078ae01db23
hack-rookie37 <dev.rookie37@gmail.com>
2023-04-13 16:41:19 +0900

    refactor: 테이블 해시 함수

--- hack-rookie37/level2.테이블 해시 함수.py
+++ hack-rookie37/level2.테이블 해시 함수.py
@@ -4,8 +4,8 @@ from functools import reduce
 def solution(data, col, row_begin, row_end):
     answer = []
     data = sorted(data, key=lambda x: (x[col - 1], -x[0]))
-
+    mod = lambda x: x % (s + 1)
     for s in range(row_begin - 1, row_end):
-        answer.append(sum(map(lambda x: x % (s + 1), data[s])))
+        answer.append(sum(map(mod, data[s])))
 
     return reduce(lambda x, y: x ^ y, answer)
