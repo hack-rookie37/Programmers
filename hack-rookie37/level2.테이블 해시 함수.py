ea49d666546aa458531b53c33a7106f5167b6452
hack-rookie37 <dev.rookie37@gmail.com>
2023-04-13 16:17:22 +0900

    refactor: 테이블 해시 함수

--- hack-rookie37/level2.테이블 해시 함수.py
+++ hack-rookie37/level2.테이블 해시 함수.py
@@ -6,6 +6,6 @@ def solution(data, col, row_begin, row_end):
     data = sorted(data, key=lambda x: (x[col - 1], -x[0]))
 
     for s in range(row_begin - 1, row_end):
-        answer.append(sum(list(map(lambda x: x % (s + 1), data[s]))))
+        answer.append(sum(map(lambda x: x % (s + 1), data[s])))
 
     return reduce(lambda x, y: x ^ y, answer)
